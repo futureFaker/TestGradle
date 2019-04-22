50cd440b26e112b12d1c57257b9a3183cc2e9951
lc254253229 <254253229@gmail.com>
2019-04-22 16:54:02 +0800

    1.TEST

---
+++ plugin1/src/main/groovy/com/lc/plugin1/MyPlugin.groovy
@@ -0,0 +1,13 @@
+package com.lc.plugin1
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        // TODO: insert action logic here
+    }
+
+}
