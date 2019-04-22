0a3cc245cee1dd33542ad3edb10b18622f1c593d
lc254253229 <254253229@gmail.com>
2019-04-22 17:25:22 +0800

    1.TEST_master

--- app/src/main/java/com/lc/testgradle/MainActivity.java
+++ app/src/main/java/com/lc/testgradle/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("-----测试分支提交---");
+        System.out.println("-----测试master提交---");
     }
 }
